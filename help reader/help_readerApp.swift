d83368affd5952ddbfed50bba5b4fdfc1f0abe1c
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2023-05-12 11:37:51 +0200

    Setting up
    Initial commit

---
+++ help reader/help_readerApp.swift
@@ -0,0 +1,17 @@
+//
+//  help_readerApp.swift
+//  help reader
+//
+//  Created by Jonatas Brisotti on 12/05/23.
+//
+
+import SwiftUI
+
+@main
+struct help_readerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
