7cf731d034185123d4e9c2a51395e8bf5d7f46cc
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2023-05-24 11:37:16 +0200

    Setting up the iOS target
    Setting up the iOS target to 16.0

--- help reader/HTMLView.swift
+++ help reader/HTMLView.swift
@@ -12,9 +12,7 @@ import WebKit
 struct HTMLView: UIViewRepresentable {
     
     @Binding var text: String
-    
-    //let htmlString: String
-    
+        
     func makeUIView(context: Context) -> WKWebView {
         //let webView =  WKWebView()
         //let request = URLRequest(url: url)
@@ -27,15 +25,3 @@ struct HTMLView: UIViewRepresentable {
     }
     
 }
-
-//struct HTMLView: UIViewRepresentable {
-//  @Binding var text: String
-//
-//  func makeUIView(context: Context) -> WKWebView {
-//    return WKWebView()
-//  }
-//
-//  func updateUIView(_ uiView: WKWebView, context: Context) {
-//    uiView.loadHTMLString(text, baseURL: nil)
-//  }
-//}
