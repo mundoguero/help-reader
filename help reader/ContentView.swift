1b8622a2ec36e8d5f02ee55c5e75fbbbbba7774c
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2023-05-26 19:41:19 +0200

    Injecting CSS
    Injecting CSS in the HTML response body

--- help reader/ContentView.swift
+++ help reader/ContentView.swift
@@ -13,54 +13,63 @@ struct ContentView: View {
     @State private var webContent: String = ""
     @State private var isLoading: Bool = false
     
-    
-     func makePOSTRequest() {
-       
-       let headers = [
-           "content-type": "application/x-www-form-urlencoded",
-           "X-RapidAPI-Key": "***REMOVED***",
-           "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
-       ]
-       
-       //Keep the word 'content=' in the beggining of the string
-       let postData = NSMutableData(data: "content=\(textToConvert)".data(using: String.Encoding.utf8)!)
-       postData.append("&response_type=html".data(using: String.Encoding.utf8)!)
-       postData.append("&request_type=html".data(using: String.Encoding.utf8)!)
-       postData.append("&fixation=1".data(using: String.Encoding.utf8)!)
-       postData.append("&saccade=10".data(using: String.Encoding.utf8)!)
-
-       let request = NSMutableURLRequest(
-           url: NSURL(
-           string: "https://bionic-reading1.p.rapidapi.com/convert"
-       )!
-           as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0
-       )
-       request.httpMethod = "POST"
-       request.allHTTPHeaderFields = headers
-       request.httpBody = postData as Data
-
-       let session = URLSession.shared
-       let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-           if (error != nil) {
-               print(error as Any)
-           } else {
-               let httpResponseHeader = response as? HTTPURLResponse
-               print(httpResponseHeader! as Any)
-               print("😜")
-               print(postData)
-               print(headers)
-               if let data = data, let httpResponseBody = String(data: data, encoding: .utf8) {
-                       print("Response data string:\n \(httpResponseBody)")
-                   DispatchQueue.main.async {
-                                           webContent = httpResponseBody
-                                           isLoading = false
-                                       }
-                   }
-           }
-       })
-       dataTask.resume()
-    
+    func makePOSTRequest() {
+        
+        let headers = [
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": "***REMOVED***",
+            "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
+        ]
+        
+        //Keep the word 'content=' in the beggining of the string
+        let postData = NSMutableData(data: "content=\(textToConvert)".data(using: String.Encoding.utf8)!)
+        postData.append("&response_type=html".data(using: String.Encoding.utf8)!)
+        postData.append("&request_type=html".data(using: String.Encoding.utf8)!)
+        postData.append("&fixation=1".data(using: String.Encoding.utf8)!)
+        postData.append("&saccade=10".data(using: String.Encoding.utf8)!)
+        
+        let request = NSMutableURLRequest(
+            url: NSURL(
+                string: "https://bionic-reading1.p.rapidapi.com/convert"
+            )!
+            as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0
+        )
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = headers
+        request.httpBody = postData as Data
+        
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error as Any)
+            } else {
+                let httpResponseHeader = response as? HTTPURLResponse
+                print(httpResponseHeader! as Any)
+                print("😜")
+                print(postData)
+                print(headers)
+                if let data = data, let httpResponseBody = String(data: data, encoding: .utf8) {
+                    print("Response data string:\n \(httpResponseBody)")
+                    DispatchQueue.main.async {
+                        let cssToAdd = """
+                        <style>
+                        body {
+                            font-size: 78px;
+                            font-family: "AmericanTypewriter"
+                        } </style>
+"""
+                        webContent = httpResponseBody + cssToAdd
+                        isLoading = false
+                    }
+                    print(webContent)
+                }
+            }
+        })
+        dataTask.resume()
+        
     }
+    
+    //        .replacingOccurrences(of: titleRegex, with: "$0" + " style=\"color: blue; font-size: 40px", options: .regularExpression, range: nil)
     
     var body: some View {
         VStack {
@@ -88,7 +97,7 @@ struct ContentView: View {
             .disabled(!textValidation())
         }
         Text("\(textToConvert)")
-        .padding()
+            .padding()
         HTMLView(text: $webContent)
         Spacer()
     }
