e82a9ecefb4a0b736ddfeef20cd3c415f4984d02
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2023-12-28 16:26:55 +0100

    Update CongratsView.swift
    Some view adjustments

--- help reader/Views/CongratsView.swift
+++ help reader/Views/CongratsView.swift
@@ -11,106 +11,104 @@ struct CongratsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        
-        VStack {
-            Spacer(minLength: 50)
-            
-            Button {
-            } label: {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 17)
-                        .frame(height: 400)
-                        .foregroundColor(Color(red: 116 / 255, green: 224 / 255, blue: 255 / 255))
-                        .shadow(radius: 2, y: 5)
-                        .padding()
-                    VStack {
-                        Text("Great!")
-                            .font(.custom("SF Pro Rounded", size: 56))
-                            .frame(width: 200, height: 30)
-                            .fontWeight(.bold)
-                            .foregroundColor(Color("FontColor"))
-                            .padding(.top, 5)
-                        LottieView(animationName: "applauso")
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 630, height: 330)
-                            .clipped()
-                            .padding(-15)
-                            .cornerRadius(17)
-                    }
-                    
-                    
-                }
-            }.padding(.horizontal, 0)
-            
-            Spacer(minLength: 25)
-            
-            HStack(spacing: 30) {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 17)
-                        .foregroundColor(Color(red: 153 / 255, green: 190 / 255, blue: 221 / 255))
-                        .shadow(radius: 2, y: 5)
-                    //.frame(width: 310, height: 390)
-                        .padding()
-                    
-                    
-                    VStack {
-                        Text("Repeat")
-                            .font(.custom("SF Pro Rounded", size: 56))
-                            .fontWeight(.heavy)
-                            .foregroundColor(Color("FontColor"))
-                            .frame(width: 250, height: 140)
-                        //.background()
-                            .multilineTextAlignment(.center)
-                            .lineLimit(2)
-                            .padding(.bottom, -5.0)
-                        Image(systemName: "gobackward")
-                            .resizable()
-                            .frame(width: 200, height: 200)
-                            .foregroundColor(Color(.white))
-                        
-                    }.padding()
-                    Spacer(minLength: 25)
-                }
-                ZStack {
-                    RoundedRectangle(cornerRadius: 17)
-                        .foregroundColor(Color(red: 153 / 255, green: 190 / 255, blue: 221 / 255))
-                        .shadow(radius: 2, y: 5)
-                        .padding()
-                    
-                    Button {
-                        presentationMode.wrappedValue.dismiss()
-                    } label: {
+        NavigationStack {
+            GeometryReader { geometry in
+                VStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 17)
+                            .foregroundColor(Color(red: 116 / 255, green: 224 / 255, blue: 255 / 255))
+                            .shadow(radius: 2, y: 5)
                         VStack {
-                            Text("Home")
-                                .font(.custom("SF Pro Rounded", size: 56))
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color("FontColor"))
-                                .frame(width: 250, height: 140)
-                            //.background()
-                                .multilineTextAlignment(.center)
-                                .lineLimit(2)
-                                .padding(.bottom, -5.0)
-                            Image(systemName: "house.fill")
-                                .resizable()
-                                .frame(width: 200, height: 200)
-                                .foregroundColor(Color(.white))
+                            Text("GREAT!")
+                                .font(.custom("SF Pro Rounded", size: geometry.size.width * 0.10))
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                                .padding(.top)
+                            Spacer()
+                            LottieView(animationName: "applauso")
+                                .padding(-50)
+                                .aspectRatio(contentMode: .fit)
+                            //.border(Color.red, width: 5)
+                                .clipped()
+                                .padding(-8)
+                            Spacer()
+                        }
+                    }.frame(width: geometry.size.width * 0.96, height: geometry.size.height * 0.38)
+                        .padding(.bottom, 30)
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 17)
+                                .foregroundColor(Color(red: 153 / 255, green: 190 / 255, blue: 221 / 255))
+                                .shadow(radius: 2, y: 5)
+                            VStack(spacing: 0) {
+                                Text("Repeat")
+                                    .font(.custom("SF Pro Rounded", size: geometry.size.width * 0.08))
+                                    .fontWeight(.bold)
+                                    .foregroundColor(Color("FontColor"))
+                                    .padding(.top)
+                                Spacer()
+                                Image(systemName: "gobackward")
+                                    .resizable()
+                                    .frame(width: geometry.size.width * 0.33,
+                                           height: geometry.size.height * 0.23)
+                                    .foregroundColor(Color(.white))
+                                    //.border(Color.red, width: 5)
+                                Spacer()
+                            }
+                        }.padding(.leading)
+                            .frame(width: geometry.size.width * 0.48,
+                                   height: geometry.size.height * 0.42)
+                        
+                        Spacer()
+                        
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 17)
+                                    .foregroundColor(Color(red: 153 / 255, green: 190 / 255, blue: 221 / 255))
+                                    .shadow(radius: 2, y: 5)
+                                VStack(spacing: 0) {
+                                    Text("Home")
+                                        .font(.custom("SF Pro Rounded", size: geometry.size.width * 0.08))
+                                        .fontWeight(.bold)
+                                        .foregroundColor(Color("FontColor"))
+                                        .padding(.top)
+                                    Spacer()
+                                    Image(systemName: "house.fill")
+                                        .resizable()
+                                        .frame(width: geometry.size.width * 0.33,
+                                               height: geometry.size.height * 0.23)
+                                        .foregroundColor(Color(.white))
+                                        //.border(Color.red, width: 5)
+                                    Spacer()
+                                }
+                            }.padding(.trailing)
+                                .frame(width: geometry.size.width * 0.48,
+                                       height: geometry.size.height * 0.42)
                             
-                        }.padding()
-                        //Spacer(minLength: 25)
+                        }
                     }
-                    //.padding(.top, -10.0)
+                    
+                    
+                    //.padding([.top, .leading, .trailing], geometry.size.width * 0.1) // Relative padding
+                    //.padding(.bottom, geometry.size.height * 0.1) // Relative padding
                 }
             }
-        }
-        .toolbar {
-            ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    self.presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("Done")
+            
+        }.background(Color("BackgroundColor")
+            .edgesIgnoringSafeArea(.all)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        self.presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Done")
+                    }
                 }
             }
-        }
+        )
     }
 }
 
