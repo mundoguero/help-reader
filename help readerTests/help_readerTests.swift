683269d689c0b4a1dc713ab1bcda1abd3a11eaf0
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2024-01-02 16:25:01 +0100

    Update help_readerTests.swift

--- help readerTests/help_readerTests.swift
+++ help readerTests/help_readerTests.swift
@@ -11,17 +11,17 @@ import XCTest
 final class help_readerTests: XCTestCase {
     
     var viewModel: MainViewModel!
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         viewModel = MainViewModel()
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         viewModel = nil
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -31,21 +31,21 @@ final class help_readerTests: XCTestCase {
     }
     
     func testPostDataBeginsWithContent() throws {
-            // Set textToConvert to a test value
-            viewModel.textToConvert = "test"
-
-            // Generate postData
-            let postData = viewModel.generatePostData()
-
-            // Test that postData begins with "content="
-            XCTAssertTrue(postData.hasPrefix("content="), "postData should begin with 'content='")
-        }
-
+        // Set textToConvert to a test value
+        viewModel.textToConvert = "test"
+        
+        // Generate postData
+        let postData = viewModel.generatePostData()
+        
+        // Test that postData begins with "content=", it's an API requirement
+        XCTAssertTrue(postData.hasPrefix("content="), "postData should start with 'content='")
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
