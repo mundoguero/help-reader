97018df31162f9cd06565ed1d1614050825e2d96
Jonatas Brisotti <jonatas.brisotti@gmail.com>
2024-01-02 16:18:48 +0100

    Refactoring MainViewModel to Enhance Testability
    Refactored MainViewModel: Extracted postData generation for improved unit testing
    
    Adding Unit Tests for MainViewModel:
    Added unit tests for MainViewModel to validate postData format
    
    Configuration Changes for Test Accessibility:
    Configured project for test target access to MainViewModel

--- help reader/Views/MainViewModel.swift
+++ help reader/Views/MainViewModel.swift
@@ -6,78 +6,79 @@
 //
 
 import Foundation
-
 import SwiftUI
 
 class MainViewModel: ObservableObject {
     @Published var textToConvert: String = "Place the txt here"
-    @Published internal var webContent: String = ""
+    @Published var webContent: String = ""
     @Published private var isLoading: Bool = false
     
-    func makePOSTRequest() {
-        
-        let headers = [
-            "content-type": "application/x-www-form-urlencoded",
-            "X-RapidAPI-Key": "***REMOVED***",
-            "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
-        ]
-        
-        //Keep the word 'content=' in the beggining of the string
+    func generatePostData() -> String {
         let postData = NSMutableData(data: "content=\(textToConvert)".data(using: String.Encoding.utf8)!)
         postData.append("&response_type=html".data(using: String.Encoding.utf8)!)
         postData.append("&request_type=html".data(using: String.Encoding.utf8)!)
         postData.append("&fixation=1".data(using: String.Encoding.utf8)!)
         postData.append("&saccade=10".data(using: String.Encoding.utf8)!)
+        return String(decoding: postData, as: UTF8.self)
+    }
+
+    func makePOSTRequest() {
+        let headers = [
+            "content-type": "application/x-www-form-urlencoded",
+            "X-RapidAPI-Key": "your-api-key",
+            "X-RapidAPI-Host": "bionic-reading1.p.rapidapi.com"
+        ]
+        
+        let postDataString = generatePostData()
+        guard let postData = postDataString.data(using: .utf8) else { return }
         
         let request = NSMutableURLRequest(
-            url: NSURL(
-                string: "https://bionic-reading1.p.rapidapi.com/convert"
-            )!
-            as URL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0
+            url: NSURL(string: "https://bionic-reading1.p.rapidapi.com/convert")! as URL,
+            cachePolicy: .useProtocolCachePolicy,
+            timeoutInterval: 10.0
         )
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = headers
-        request.httpBody = postData as Data
-        
+        request.httpBody = postData
+
         let cssToAdd =
         """
-                                            <style>
-                                        body {
-                                        color: black;
-                                        font-size: 28px;
-                                        font-family: "-apple-system";
-                                            } </style>
-                """
-        
+        <style>
+        body {
+            color: black;
+            font-size: 28px;
+            font-family: "-apple-system";
+        } </style>
+        """
+
         let session = URLSession.shared
-        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
-            if (error != nil) {
-                print(error as Any)
-            } else {
-                let httpResponseHeader = response as? HTTPURLResponse
-                print(httpResponseHeader! as Any)
+        let dataTask = session.dataTask(with: request as URLRequest) { [weak self] (data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+
+            if let httpResponseHeader = response as? HTTPURLResponse {
+                print(httpResponseHeader)
                 print("😜")
                 print(postData)
                 print(headers)
                 if let data = data, let httpResponseBody = String(data: data, encoding: .utf8) {
                     print("Response data string:\n \(httpResponseBody)")
                     DispatchQueue.main.async {
-                        self.webContent = httpResponseBody + cssToAdd
-                        self.isLoading = false
+                        self?.webContent = httpResponseBody + cssToAdd
+                        self?.isLoading = false
                     }
-                    print(self.webContent)
+                    print(self?.webContent ?? "")
                 }
             }
-        })
-        dataTask.resume()
-        
-    }
-    
-    func textValidation() -> Bool {
-        if textToConvert.count >= 3 {
-            return true
         }
-        return false
+        dataTask.resume()
+    }
+
+    func textValidation() -> Bool {
+        return textToConvert.count >= 3
     }
 }
+
 
---
+++ help readerTests/help_readerTests.swift
@@ -0,0 +1,51 @@
+//
+//  help_readerTests.swift
+//  help readerTests
+//
+//  Created by Jonatas Brisotti on 02/01/24.
+//
+
+import XCTest
+@testable import help_reader
+
+final class help_readerTests: XCTestCase {
+    
+    var viewModel: MainViewModel!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewModel = MainViewModel()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        viewModel = nil
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+    
+    func testPostDataBeginsWithContent() throws {
+            // Set textToConvert to a test value
+            viewModel.textToConvert = "test"
+
+            // Generate postData
+            let postData = viewModel.generatePostData()
+
+            // Test that postData begins with "content="
+            XCTAssertTrue(postData.hasPrefix("content="), "postData should begin with 'content='")
+        }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
